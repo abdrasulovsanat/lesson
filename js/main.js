d02959312f9c8d4b300dfd47748577b70da74bb5
abdrasulovsanat <abdrasulovsanat@gmail.com>
2022-12-13 16:47:18 +0600

    hit

--- js/main.js
+++ js/main.js
@@ -98,3 +98,91 @@
 //         console.log(arr[i][j]);
 //     };
 // };
+
+// Задание №1
+// Дан массив ["Polina", "Mirdin", "Azret", "Aliia", "Aidana"]. Выведите в консоль имена Mirdin и Aliia с помощью цикла for
+
+let arr = ['Polina', 'Mirdin', 'Azret', 'Aliia', 'Aidana'];
+for (let i = 0; i < arr.length; i += 1) {
+  if (i % 2 != 0) {
+    console.log(arr[i]);
+  }
+}
+
+// Задание №2
+// Создайте пустой массив и заполните его числами от 1 до 10 с помощью цикла for
+
+let res = [];
+for (let l = 1; l < 11; l++) {
+  res.push(l);
+}
+console.log(res);
+
+// Задание №3
+// Выведите в консоль все числа в промежутке с 50 до 99 включительно с помощью цикла.
+
+let res1 = [];
+for (let k = 50; k < 100; k++) {
+  res1.push(k);
+}
+console.log(res1);
+
+// Задание №4
+// Выведите те же числа, но уже в обратном порядке, т.е. с 99 до 50.
+
+let res2 = [];
+for (let t = 50; t < 100; t++) {
+  res2.push(t);
+}
+res2.sort((a, b) => b - a);
+console.log(res2);
+
+// Задание №5
+// Найдите самый длинный элемент массива и вывкдите в консоль
+// ["pen", "pineapple", "apple", "pen"]
+
+let arr2 = ['pen', 'pineapple', 'apple', 'pen'];
+let updArr2 = [];
+for (let j = 0; j < arr2.length; j++) {
+  updArr2.push(arr2[j]);
+}
+updArr2 = arr2.sort((a, b) => b.length - a.length);
+console.log(updArr2[0]);
+
+// Задание №6
+// Так же найдите самый короткий элемент этого же массива
+
+let arr3 = ['pen', 'pineapple', 'apple', 'pen'];
+let updArr3 = [];
+for (let s = 0; s < arr3.length; s++) {
+  updArr3.push(arr3[s]);
+}
+updArr3 = arr3.sort((a, b) => a.length - b.length);
+console.log(updArr3[0], updArr3[1]);
+
+// Задание №7
+// Выведите в консоль все элементы с четными индексами.
+// [1,2,3,4,5,6,7,8,9]
+
+let arr4 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+for (let p = 2; p <= 9; p++) {
+  if (p % 2 == 0) {
+    console.log(p);
+  }
+}
+
+// Задание №8
+// Дан двумерный массив. Выведите в консоль все элементы.
+// [[1,2], [3,4], [5,6]]
+
+let arr5 = [
+  [1, 2],
+  [3, 4],
+  [5, 6],
+];
+
+for (let o = 0; o < arr5.length; o++) {
+  for (let u = 0; u < arr5[o].length; u++) {
+    console.log(arr5[o][u]);
+  }
+}
